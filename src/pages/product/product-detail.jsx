862c55346f4159d4d951064805d94bb6575f5925
Lê Quốc Anh <21520565@gm.uit.edu.vn>
2024-06-27 14:08:17 +0700

    fix product detail

--- src/pages/product/product-detail.jsx
+++ src/pages/product/product-detail.jsx
@@ -1,22 +1,21 @@
 import "./product-detail.scss";
 import "./payment.scss";
-import {Navigation, Pagination} from "swiper";
+import { Pagination, Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import {Swiper, SwiperSlide} from "swiper/react";
-import React, {useEffect, useState} from "react";
-import {useNavigate, useParams} from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import http from "../../utils/request";
 import Counter from "./counter";
 import Swal from "sweetalert2";
-
 import { useDispatch, useSelector } from "react-redux";
 import { setCartAction } from "../../store/actions";
 
 const ProductDetail = (props) => {
-  const {productInfo} = window.location.state || {};
-  const {id} = useParams();
+  const { productInfo } = window.location.state || {};
+  const { id } = useParams();
   const navigate = useNavigate();
   const [product, setProduct] = useState(null);
   const [imageSelected, setImageSelected] = useState(null);
@@ -24,12 +23,13 @@ const ProductDetail = (props) => {
   const [error, setError] = useState(null);
   const [selectedOptions, setSelectedOptions] = useState({});
   const [quantity, setQuantity] = useState(1); // Initial quantity
-
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cartReducer.cart);
   
+
+
   const handleOptionChange = (optionName, optionValue) => {
-    setSelectedOptions({...selectedOptions, [optionName]: optionValue});
+    setSelectedOptions({ ...selectedOptions, [optionName]: optionValue });
   };
 
   const handleQuantityChange = (newQuantity) => {
@@ -123,22 +123,6 @@ const ProductDetail = (props) => {
     });
   };
 
-  /**
-   * handle buy now
-   */
-  const handleBuyNow = React.useCallback(() => {
-    addItemToCart({quantity, model: product.model})
-    navigate(`/checkout`, {state: {}});
-  }, [addItemToCart, quantity, product?.model])
-
-  /**
-   * handle add to cart
-   * @type {(function())|*}
-   */
-  const handleAddToCart = React.useCallback(() => {
-    addItemToCart({quantity, model: product.model})
-  }, [product, addItemToCart])
-
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -147,134 +131,108 @@ const ProductDetail = (props) => {
     return <div>Error: {error}</div>;
   }
 
-
   return (
-      <>
-        <div className="container mt-3">
-          <div className="row">
-            <div className="col-7">
-              <div className="card">
-                <div className="card-body">
-                  <div className="" style={{width: "50vw", height: "60vh"}}>
-                    <img
-                        class="hover-c"
-                        src={`https://media-api-beta.thinkpro.vn/${imageSelected}`}
-                        alt=""
+    <>
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-7">
+            <div className="card">
+              <div className="card-body">
+                <div className="" style={{ width: "50vw", height: "60vh" }}>
+                  <img
+                    class="hover-c"
+                    src={`https://media-api-beta.thinkpro.vn/${imageSelected}`}
+                    alt=""
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "contain",
+                    }}
+                    onClick={() =>
+                      handlePreviewImage(
+                        `https://media-api-beta.thinkpro.vn/${imageSelected}`
+                      )
+                    }
+                  />
+                </div>
+                <hr />
+                <Swiper
+                  slidesPerView={6}
+                  spaceBetween={30}
+                  pagination={{
+                    clickable: true,
+                    type: "ver",
+                  }}
+                  navigation={true}
+                  modules={[Pagination, Navigation]}
+                >
+                  {product.assets.map((image) => (
+                    <SwiperSlide
+                      key={image.src}
+                      onClick={() => setImageSelected(image.src)}
+                    >
+                      <div
+                        className={
+                          image.src === imageSelected
+                            ? "img-selected  hover-c"
+                            : "hover-c"
+                        }
                         style={{
+                          backgroundImage: `url(${image.src})`,
+                          backgroundSize: "cover",
+                          backgroundPosition: "center",
                           width: "100%",
                           height: "100%",
-                          objectFit: "contain",
                         }}
-                        onClick={() =>
-                            handlePreviewImage(
-                                `https://media-api-beta.thinkpro.vn/${imageSelected}`
-                            )
-                        }
-                    />
-                  </div>
-                  <hr/>
-                  <Swiper
-                      slidesPerView={6}
-                      spaceBetween={30}
-                      pagination={{
-                        clickable: true,
-                        type: "ver",
-                      }}
-                      navigation={true}
-                      modules={[Pagination, Navigation]}
-                  >
-                    {product.assets.map((image) => (
-                        <SwiperSlide
-                            key={image.src}
-                            onClick={() => setImageSelected(image.src)}
-                        >
-                          <div
-                              className={
-                                image.src === imageSelected
-                                    ? "img-selected  hover-c"
-                                    : "hover-c"
-                              }
-                              style={{
-                                backgroundImage: `url(${image.src})`,
-                                backgroundSize: "cover",
-                                backgroundPosition: "center",
-                                width: "100%",
-                                height: "100%",
-                              }}
-                          >
-                            <img
-                                src={`https://images.thinkgroup.vn/unsafe/212x212/https://media-api-beta.thinkpro.vn/${image.src}`}
-                                alt="section-banner"
-                            />
-                          </div>
-                        </SwiperSlide>
-                    ))}
-                  </Swiper>
-                </div>
+                      >
+                        <img
+                          src={`https://images.thinkgroup.vn/unsafe/212x212/https://media-api-beta.thinkpro.vn/${image.src}`}
+                          alt="section-banner"
+                        />
+                      </div>
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
               </div>
             </div>
-            <div className="col-5">
-              <div className="card">
-                <div className="card-body">
-                  <p>SKU: {product.SKU}</p>
-                  <p>
-                    <strong>{product.model.name}</strong>
-                  </p>
-                  <hr/>
-                  <div className="variable" style={{fontSize: "14px"}}>
-                    {product.variations.map((item) => (
-                        <div className="variable" key={item.name}>
-                          <h3 style={{fontSize: "16px", opacity: 0.9}}>
-                            {item.label}
-                          </h3>
+          </div>
+          <div className="col-5">
+            <div className="card">
+              <div className="card-body">
+                <p>SKU: {product.SKU}</p>
+                <p>
+                  <strong>{product.model.name}</strong>
+                </p>
+                <hr />
+                <div className="variable" style={{ fontSize: "14px" }}>
+                  {product.variations.map((item) => (
+                    <div className="variable" key={item.name}>
+                      <h3 style={{ fontSize: "16px", opacity: 0.9 }}>
+                        {item.label}
+                      </h3>
+                      <div
+                        className="d-flex flex-wrap gx-3 gy-3"
+                        style={{ margin: 0 }}
+                      >
+                        {item.options.map((option) => (
                           <div
-                              className="d-flex flex-wrap gx-3 gy-3"
-                              style={{margin: 0}}
+                            className="alert alert-secondary bg-white text-center m-1 px-3 py-1"
+                            key={option.name}
+                            onClick={() =>
+                              handleOptionChange(item.name, option.name)
+                            }
                           >
-                            {item.options.map((option) => (
-                                <div
-                                    className="alert alert-secondary bg-white text-center m-1 px-3 py-1"
-                                    key={option.name}
-                                    onClick={() =>
-                                        handleOptionChange(item.name, option.name)
-                                    }
-                                >
-                                  {option.name}
-                                </div>
-                            ))}
+                            {option.name}
                           </div>
-                        </div>
-                    ))}
-                  </div>
-                  <div className="counter mt-3">
-                    <h3 style={{fontSize: "16px", opacity: 0.9}}>Số lượng</h3>
-                    <div className="d-flex">
-                      <Counter onQuantityChange={val => setQuantity(val)}/>
+                        ))}
+                      </div>
                     </div>
-                  </div>
-                  <hr/>
+                  ))}
+                </div>
+                <div className="counter mt-3">
+                  <h3 style={{ fontSize: "16px", opacity: 0.9 }}>Số lượng</h3>
                   <div className="d-flex">
-                    <p
-                        style={{
-                          fontSize: "1.6em",
-                          color: "red",
-                          fontWeight: "bold",
-                        }}
-                    >
-                      {product.model.price.toLocaleString("it-IT", {
-                        style: "currency",
-                        currency: "VND",
-                      })}
-                    </p>
-                    <div className="spacer"></div>
-                    <button type='button' className="btn btn-add-cart" onClick={handleAddToCart}>Thêm vào giỏ</button>
-                    <button type='button'
-                            className="btn btn-danger"
-                            style={{marginLeft: "8px", fontWeight: "bold"}}
-                            onClick={handleBuyNow}
-                    >
-                      Mua ngay
-                    </button>
+                    <Counter />
                   </div>
                 </div>
                 <hr />
@@ -304,53 +262,54 @@ const ProductDetail = (props) => {
               </div>
             </div>
           </div>
+        </div>
 
-          <div className="row">
-            <div className="card mt-5">
-              <div className="card-body">
-                <h2 style={{fontSize: "1.6em"}}>Cấu hình đặc điểm</h2>
-                <div className="row" style={{fontSize: "0.9em"}}>
-                  {product.attributes.map((attribute) => (
-                      <div className="col-6 mt-3" key={attribute.groupName}>
-                        <p>
-                          <strong>{attribute.groupName}</strong>
-                        </p>
-                        {attribute.items.map((item) => (
-                            <p key={item.label}>
-                              <span>{item.label}</span> : <span> {item.value}</span>
-                            </p>
-                        ))}
-                      </div>
-                  ))}
-                </div>
-                <hr/>
-                <h2 style={{fontSize: "1.6em"}}>Bảo hành đổi trả</h2>
-                <ul>
-                  <li>
-                    Bảo hành <strong>12 tháng tại chuỗi cửa hàng</strong>
-                  </li>
-                  <li>Đổi mới trong 15 ngày đầu tiên</li>
-                </ul>
-                <hr/>
-                <h2 style={{fontSize: "1.6em"}}>Bài viết mô tả</h2>
-                {product.article ? (
-                    <section className="section-article">
-                      <h1>{product.article.title}</h1>
-                      <p>{product.article.description}</p>
-                      <div
-                          dangerouslySetInnerHTML={{
-                            __html: product.article.content,
-                          }}
-                      />
-                    </section>
-                ) : (
-                    "-- chưa có mô tả --"
-                )}
+        <div className="row">
+          <div className="card mt-5">
+            <div className="card-body">
+              <h2 style={{ fontSize: "1.6em" }}>Cấu hình đặc điểm</h2>
+              <div className="row" style={{ fontSize: "0.9em" }}>
+                {product.attributes.map((attribute) => (
+                  <div className="col-6 mt-3" key={attribute.groupName}>
+                    <p>
+                      <strong>{attribute.groupName}</strong>
+                    </p>
+                    {attribute.items.map((item) => (
+                      <p key={item.label}>
+                        <span>{item.label}</span> : <span> {item.value}</span>
+                      </p>
+                    ))}
+                  </div>
+                ))}
               </div>
+              <hr />
+              <h2 style={{ fontSize: "1.6em" }}>Bảo hành đổi trả</h2>
+              <ul>
+                <li>
+                  Bảo hành <strong>12 tháng tại chuỗi cửa hàng</strong>
+                </li>
+                <li>Đổi mới trong 15 ngày đầu tiên</li>
+              </ul>
+              <hr />
+              <h2 style={{ fontSize: "1.6em" }}>Bài viết mô tả</h2>
+              {product.article ? (
+                <section className="section-article">
+                  <h1>{product.article.title}</h1>
+                  <p>{product.article.description}</p>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: product.article.content,
+                    }}
+                  />
+                </section>
+              ) : (
+                "-- chưa có mô tả --"
+              )}
             </div>
           </div>
         </div>
-      </>
+      </div>
+    </>
   );
 };
 
